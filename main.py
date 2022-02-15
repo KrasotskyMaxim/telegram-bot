d4f7162afc80831f16f99c9a56d2646dba94f9f4
Maxim Krasotsky <krasotsky2003@gmail.com>
2022-02-15 22:17:20 +0300

    load json bd and update code

--- main.py
+++ main.py
@@ -16,54 +16,48 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 RANDOM_STATE = 42
+BOT_CONFIG = {}
+RUSSIAN_LETTERS = ['ё','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','ф','ы','в','а','п','р','о','л','д','ж','э','я','ч','с','м','и','т','ь','б','ю']
 
-BOT_CONFIG = {
-    "intents": {
-        "hello": {
-            "examples": ["Hi", "Hello", "Good evening"],
-            "responses": ["Hi", "Hello", "Good evening"],
-        },
-        "thanks": {
-            "examples": ["Thank you", "Thanks", "Thank you very much"],
-            "responses": ["You are Welcome", "Ok", "Contact again"],
-        },
-        "goodbuy": {
-            "examples": ["Buy", "Goodbuy", "See you"],
-            "responses": ["Goodbuy", "Buy", "See you later"],
-        },
-        "good": {
-            "examples": ["Good", "Ok", "Very well"],
-            "responses": ["Very well", "Good!", "Well"],
-        },
-        "bad": {
-            "examples": ["Bad", "Very bad", "Poorly"],
-            "responses": ["Sorry", "I apologize", "Excuse me"],
-        },
-    }
-}
+
+with open("BOT_CONFIG.json", 'r', encoding="utf-8") as f:
+    BOT_CONFIG = json.load(f)
+    #print(BOT_CONFIG["intents"].keys())
+
+# BOT_CONFIG = {
+#     "intents": {
+#         "hello": {
+#             "examples": ["Hi", "Hello", "Good evening"],
+#             "responses": ["Hi", "Hello", "Good evening"],
+#         },
+#         "thanks": {
+#             "examples": ["Thank you", "Thanks", "Thank you very much"],
+#             "responses": ["You are Welcome", "Ok", "Contact again"],
+#         },
+#         "goodbuy": {
+#             "examples": ["Buy", "Goodbuy", "See you"],
+#             "responses": ["Goodbuy", "Buy", "See you later"],
+#         },
+#         "good": {
+#             "examples": ["Good", "Ok", "Very well"],
+#             "responses": ["Very well", "Good!", "Well"],
+#         },
+#         "bad": {
+#             "examples": ["Bad", "Very bad", "Poorly"],
+#             "responses": ["Sorry", "I apologize", "Excuse me"],
+#         },
+#     }
+# }
 
 
 def clean_text(text):
     output_text = ""
     for ch in text:
-        if ch in ascii_letters:
+        if ch in ascii_letters or ch in RUSSIAN_LETTERS:
             output_text += ch
     return output_text
 
-
-def get_intent(input_text, model, vectorizer):
-    return model.predict(vectorizer.transform([input_text]))[0]
-
-
-def bot(input_text, model, vectorizer):
-    intent = get_intent(input_text, model, vectorizer)
-    return random.choice(BOT_CONFIG["intents"][intent]["responses"])
-
 def main():
-    # with open("BOT_CONFIG.json", 'r') as f:
-    #     BOT_CONFIG = json.load(f)
-    #print(BOT_CONFIG["intents"].keys())
-
     X = []
     y = []
 
@@ -86,7 +80,7 @@ def main():
     # model = LogisticRegression(random_state=42, C=10)
     model = RandomForestClassifier(random_state=RANDOM_STATE, n_estimators=200) 
     model.fit(X_train_vectorized, y_train)
-    print(model.predict(vectorizer.transform(["say hello"])))
+    #print(model.predict(vectorizer.transform(["say hello"])))
 
     # LogReg - 1.0
     # LogReg Train - 0.9
@@ -108,9 +102,27 @@ def main():
         else:
             bot_answer = bot(clean_text(input_text), model, vectorizer)
             print(bot_answer)
-            if bot_answer in BOT_CONFIG["intents"]["goodbuy"]["responses"]:
+            if leave(bot_answer):
                 break
+
+def leave(bot_answer):
+    exit_intents = ["bye", "goodbye"]
+    for intent in exit_intents:
+        if bot_answer in BOT_CONFIG["intents"][intent]["responses"]:
+            return True
+    return False
+        
+
+def get_intent(input_text, model, vectorizer):
+    return model.predict(vectorizer.transform([input_text]))[0]
+
+
+def bot(input_text, model, vectorizer):
+    intent = get_intent(input_text, model, vectorizer)
+    return random.choice(BOT_CONFIG["intents"][intent]["responses"])
 
 
 if __name__ == "__main__":
     main()
+
+
